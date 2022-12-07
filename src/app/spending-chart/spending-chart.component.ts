6f47c377830659fe13fe1f09eff978be40e36edc
alhammialiff <alhammi.aliff@gmail.com>
2022-12-07 19:56:59 +0800

    Added mouseover highlight on bar chart

--- src/app/spending-chart/spending-chart.component.ts
+++ src/app/spending-chart/spending-chart.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { SpendingService } from '../services/spending.service';
 import { Spending } from '../shared/spending';
-import * as d3 from 'd3-selection';
-import * as d3Scale from 'd3-scale';
-import * as d3Shape from 'd3-shape';
-import * as d3Array from 'd3-array';
-import * as d3Axis from 'd3-axis';
+import * as d3 from 'd3';
+// import * as d3Select from 'd3-selection';
+// import * as d3Scale from 'd3-scale';
+// import * as d3Shape from 'd3-shape';
+// import * as d3Array from 'd3-array';
+// import * as d3Axis from 'd3-axis';
 
 @Component({
   selector: 'app-spending-chart',
@@ -25,6 +26,7 @@ export class SpendingChartComponent implements OnInit {
         this.createSVG();
         console.log(this.spending);
         this.drawBars(this.spending);
+        this.highlightBar();
       });
 
   }
@@ -35,35 +37,37 @@ export class SpendingChartComponent implements OnInit {
   private height = 400 - (this.margin * 2);
 
   private createSVG(): void {
+
     this.svg = d3.select('figure#bar')
       .append('svg')
       .attr('width', this.width + (this.margin * 2))
       .attr('height', this.height + (this.height * 2))
       .append("g")
       .attr("transform", "translate(" + this.margin + "," + this.margin + ")");
+      
   }
 
   private drawBars(data: any[]): void {
 
-    const x = d3Scale.scaleBand()
+    const x = d3.scaleBand()
       .range([0, this.width])
       .domain(data.map(d => d.date))
 
     this.svg.append("g")
       .attr("transform", "translate(0," + this.height + ")")
-      .call(d3Axis.axisBottom(x))
+      .call(d3.axisBottom(x))
       .selectAll("text")
       .attr("transform", "translate(-10,0)rotate(-45)")
       .style("text-anchor", "end");
 
-    const y = d3Scale.scaleLinear()
+    const y = d3.scaleLinear()
       .domain([0, 2000])
       .range([this.height, 0]);
 
     this.svg.append("g")
-      .call(d3Axis.axisLeft(y));
+      .call(d3.axisLeft(y));
 
-    this.svg.selectAll("bar")
+    this.svg.selectAll("rect")
       .data(data)
       .enter()
       .append("rect")
@@ -72,6 +76,21 @@ export class SpendingChartComponent implements OnInit {
       .attr("width", x.bandwidth())
       .attr("height", (d) => this.height - y(d.thisMonth))
       .attr("fill", "#d04a35");
+  }
+
+  private highlightBar(): void{
+    d3.selectAll("rect")
+      .on("mouseover", function(){
+        d3.select(this)
+          .style("fill", "rgb(230, 74, 53)");
+
+          console.log("Click");
+
+      })
+      .on("mouseout", function(){
+        d3.select(this)
+          .style("fill", "rgb(208, 74, 53)")
+      });
   }
 
 }
