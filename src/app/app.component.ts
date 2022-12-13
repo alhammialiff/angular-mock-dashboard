66af73b9a63d2c958dd0062162bafc37e47eb10c
alhammialiff <alhammi.aliff@gmail.com>
2022-12-13 12:38:23 +0800

    Added Media Query observer to observe on the changing screen size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MediaChange, MediaService } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-mock-dashboard';
+  mediaSub: Subscription;
+  deviceXs: boolean;
+  
+  constructor(public mediaService: MediaService){}
+
+  ngOnInit(){
+    this.mediaSub = this.mediaService.subscribe((res: MediaChange)=>{
+      console.log(res.mqAlias);
+      this.deviceXs = res.mqAlias === "xs" ? true : false;
+    })
+  }
+
+  ngOnDestroy() {
+    this.mediaSub.unsubscribe();
+  }
+
 }
